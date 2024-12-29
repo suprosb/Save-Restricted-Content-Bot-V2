d6e718cd4716f7b250c6c7e8d5f0ced8dd5e8e73
suprosb <suproboiragi1@gmail.com>
2024-12-29 22:06:34 +0600

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -166,7 +166,7 @@ async def terms(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/now_botupdate")],
         ]
     )
     await message.reply_text(terms_text, reply_markup=buttons)
@@ -185,7 +185,7 @@ async def plan(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/now_botupdate")],
         ]
     )
     await message.reply_text(plan_text, reply_markup=buttons)
@@ -222,7 +222,7 @@ async def see_terms(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/now_botupdate")],
         ]
     )
     await callback_query.message.edit_text(terms_text, reply_markup=buttons)
